870d0713c8622055ab69d3f7a2c416c6bb2938fd
Zakariya <pzakariya@mail.ru>
2025-02-14 08:31:46 +0500

    Добавил основной файл

--- run.py
+++ run.py
@@ -1,23 +1,23 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
-from aiogram.types import Message
 
 from config import TOKEN
 
 import logging
 
+from app.handlers import handlers
+from app.handlers import handler_webenar
+
+logging.basicConfig(level=logging.INFO)
+
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
-@dp.message(CommandStart())
-async def cmd_start(message: Message):
-    await message.answer(f"Привет, {message.from_user.first_name}! Это тестовый бот PHCOS.")
-
-
 async def main():
+    dp.include_router(handlers.router)
+    dp.include_router(handler_webenar.router)
     await dp.start_polling(bot)
 
 
