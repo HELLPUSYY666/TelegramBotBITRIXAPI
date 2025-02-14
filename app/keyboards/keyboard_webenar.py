68e98badd6b2d452c1b41305a12248c91f1d9891
Zakariya <pzakariya@mail.ru>
2025-02-14 08:31:26 +0500

    Добавил клаву вебенары

---
+++ app/keyboards/keyboard_webenar.py
@@ -0,0 +1,16 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+confirm_kb = ReplyKeyboardMarkup(
+    keyboard=[[KeyboardButton(text="✅ Подтвердить регистрацию")]],
+    resize_keyboard=True
+)
+
+main_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="📅 Регистрация на вебинар")],
+        [KeyboardButton(text="📆 Расписание"), KeyboardButton(text="📦 Каталог товаров")],
+        [KeyboardButton(text="🔙 Назад")]
+    ],
+    resize_keyboard=True
+)
